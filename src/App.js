2577910c1d15e28275eb0e33f3dcefaca81da4b2
Shafkat <kabirmd@msu.edu>
2023-11-23 20:38:03 -0500

    twinkling background homepage

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;1,400&display=swap"
+          rel="stylesheet"
+        />
+        {/* You don't need to import style.css here, React will handle it */}
+      </head>
+      <body>
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="clouds"></div>
+        <div className="title">
+          <h1>Shafkat Kabir</h1>
+          <h2>Aspiring Software Engineer</h2>
+        </div>
+      </body>
+      <body>
+        Hi
+      </body>
+    </div>
+  );
+}
+
+export default App;
