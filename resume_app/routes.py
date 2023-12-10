05bbd0bf1c9610e72299623d6a9a999775a42c0c
Shafkat <kabirmd@msu.edu>
2023-12-10 06:34:13 -0500

    changes to repo

--- resume_app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import current_app as app
-from flask import render_template, redirect
-from pprint import pprint
-
-@app.route('/')
-def index():
-    return render_template('home.html')
